e73f21faa6917c2710797709382fa15f24b065f5
chiseanchang0727 <sd2fsha627@gmail.com>
2024-12-20 10:51:50 +0800

    add lora config explanation

--- fine-tuning/ft_configs.py
+++ fine-tuning/ft_configs.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import BitsAndBytesConfig
 from peft import LoraConfig
+import bitsandbytes as bnb
 
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
@@ -11,7 +12,7 @@ bnb_config = BitsAndBytesConfig(
 
 
 lora_config = LoraConfig(
-    r=16,
+    r=16,  # The rank of the low-rank decomposition in LoRA (Low-Rank Adaptation)
     lora_alpha=16,
     target_modules=[
         'q_proj',
@@ -22,7 +23,24 @@ lora_config = LoraConfig(
         'up_proj',
         'down_proj'
     ],
-    bias='none',
+    bias='none', # Specifies whether and how biases are handled in LoRA.
     lora_dropout=0.05,
     task_type='CAUSAL_LM'
 )
+
+
+
+
+
+
+def find_all_linear_names(model):
+    lora_module_names = set()
+    for name, module in model.named_modules():
+        if isinstance(module, bnb.nn.Linear4bit):
+            names = name.split(".")
+            # model-specific
+            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+
+    if "lm_head" in lora_module_names:  # needed for 16-bit
+        lora_module_names.remove("lm_head")
+    return list(lora_module_names)
